850bd24534b1e9865bbbdc8ad40dab8bea15c8af
IgorLohan <44535099+IgorLohan@users.noreply.github.com>
2022-10-04 14:47:49 -0300

    adicionando mes e estilizando css

--- script.js
+++ script.js
@@ -6,18 +6,24 @@ function contar() {
     //Calculando tempo restante
     var dataAtual = new Date().getTime();
     var x = dataDate - dataAtual;
-    var dia = Math.floor(x / (1000*60*60*24));
-    var hora = Math.floor((x % (1000*60*60*24)) / (1000*60*60));
-    var minutos = Math.floor((x % (1000*60*60)) / (1000*60));
-    var segundos = Math.floor((x % (1000*60)) / 1000);
+    var mes = Math.floor(x / (1000 * 60 * 60 * 24 * 30));
+    var dias = Math.floor(x / (1000 * 60 * 60 * 24));
+    var horas = Math.floor((x % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutos = Math.floor((x % (1000 * 60 * 60)) / (1000 * 60));
+    var segundos = Math.floor((x % (1000 * 60)) / 1000);
 
     //Imprimindo contagem regressiva para o evento
-    document.getElementById("demo").innerHTML = dia + "d " + hora + "h " + minutos + "m " + segundos + "s ";
+    document.getElementById("mes").innerHTML = mes;
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
 
     //Mensagem ao termino do tempo
     if (x < 0) {
-      document.getElementById("demo").innerHTML = "Seu evento começou!";
+      document.getElementById("clockdiv").innerHTML = "Seu evento começou!";
       clearInterval(intervalo);
     }
   }, 1000);
 }
+
