cbd45f6106dd8402f96317c1e64d91e3e6f5b9d2
IgorLohan <44535099+IgorLohan@users.noreply.github.com>
2022-10-03 23:01:36 -0300

    js funcionando

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-function contar(){
-    var data = new Date("dec 31, 2023 15:37:25").getTime();
-    //document.getElementById("data").getTime()
+function contar() {
+  let intervalo = setInterval(() => {
+    var data = document.getElementById("data").value.split("/");
+    var dataDate = new Date(data[2],data[1]-1,data[0]).getTime();
 
     //Calculando tempo restante
     var dataAtual = new Date().getTime();
-    var x = data - dataAtual;
+    var x = dataDate - dataAtual;
     var dia = Math.floor(x / (1000*60*60*24));
-    var hora = Math.floor((t % (1000*60*60*24)) / (1000*60*60));
-    var minutos = Math.floor((t % (1000*60*60)) / (1000*60));
-    var segundos = Math.floor((t % (1000*60)) / 1000);
+    var hora = Math.floor((x % (1000*60*60*24)) / (1000*60*60));
+    var minutos = Math.floor((x % (1000*60*60)) / (1000*60));
+    var segundos = Math.floor((x % (1000*60)) / 1000);
 
     //Imprimindo contagem regressiva para o evento
     document.getElementById("demo").innerHTML = dia + "d " + hora + "h " + minutos + "m " + segundos + "s ";
 
     //Mensagem ao termino do tempo
-    if(x < 0){
-        document.getElementById("demo").innerHTML = "Seu evento começou!";
+    if (x < 0) {
+      document.getElementById("demo").innerHTML = "Seu evento começou!";
+      clearInterval(intervalo);
     }
-
+  }, 1000);
 }
